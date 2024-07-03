8e5d51b9c21dd3e740c499ca4ec583e9100bbaee
Adhishdevadas <2222ja03@kgcas.com>
2024-07-03 13:05:34 +0530

    03/07/24

---
+++ JS/script8.js
@@ -0,0 +1,19 @@
+// function display(result){
+//     console.log(result);
+//   }
+  
+//   function calculation(num1, num2, callback){
+//     let total = num1+num2;
+//     callback(total);
+//   }
+
+//   calculation(10,20,display)
+
+function add(num) {
+   console.log(num);
+    if () {
+        
+    }
+   add(++num); 
+}
+add(1);
