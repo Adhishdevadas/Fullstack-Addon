a53eed2ef88ba84d1ff1c58b36b97f0781439bd2
Adhishdevadas <2222ja03@kgcas.com>
2024-07-03 13:07:00 +0530

    03/12/24

--- JS/script8.js
+++ JS/script8.js
@@ -9,11 +9,35 @@
 
 //   calculation(10,20,display)
 
-function add(num) {
-   console.log(num);
-    if () {
+// function add(num) {
+//    console.log(num);
+//     if (num<10) {
+//         add(++num); 
         
-    }
-   add(++num); 
+//     }
+// }
+// add(1);
+
+// function start() {
+//     console.log("start");
+// }
+// setInterval(start,5000);
+
+// function runing() {
+//     console.log("runing");
+// }
+// runing();
+
+// let num=1;
+// function start() {
+//     let d= new Date();
+//     document.getElementById("time").innerHTML=d;
+// }
+// setInterval(start,1000)
+
+async function data() {
+    await fetch('https://fakestoreapi.com/products/1')
+    .then(res=>res.json())
+    .then(json=>console.log(json))
 }
-add(1);
+data();
